84bfa765a026ab954110f3a28a5cf48b38044ca0
RuiyanH <54792448+RuiyanH@users.noreply.github.com>
2025-08-12 18:54:12 +0000

    Deploying to gh-pages from @ RuiyanH/ruiyanh.github.io@3a81ae5ad08bce99dc5ec83da82b3abdd25c5230 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -62,6 +62,17 @@ ninja.data = [{
             window.location.href = "/blog/2024/photo-gallery/";
           
         },
+      },{id: "post-the-role-of-carbon-markets-in-achieving-net-zero-emissions",
+        
+          title: "The Role of Carbon Markets in Achieving Net-Zero Emissions",
+        
+        description: "As the world races toward net-zero, carbon markets are key—but are AI-driven solutions the fix or just a temporary Band-Aid for deeper systemic issues?",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2024/math/";
+          
+        },
       },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
         
           title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
@@ -346,17 +357,6 @@ ninja.data = [{
         handler: () => {
           
             window.location.href = "/blog/2020/twitter/";
-          
-        },
-      },{id: "post-a-post-with-math",
-        
-          title: "a post with math",
-        
-        description: "an example of a blog post with some math",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2015/math/";
           
         },
       },{id: "books-the-godfather",
